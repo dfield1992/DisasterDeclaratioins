bca7b48e60f6ce3246038478629e4bf6ecf51191
dfield1992 <44931591+dfield1992@users.noreply.github.com>
2019-07-15 20:57:23 -0500

    Second Push

--- js/main.js
+++ js/main.js
@@ -1,24 +1,18 @@
-/* Map of GeoJSON data from MidWest.geojson */
-
 //function to instantiate the Leaflet map
 function createMap(){
     //create the map
     var map = L.map('map', {
-        center: [40.00, -91.20],
-        zoom: 5
-    
+        center: [40.00, -91.20],//Coordinated to center the map for Midwestern States
+        zoom: 5    
     });
     
-
-//searchLayer is a L.LayerGroup contains searched markers
-//add tile layer...replace project id and accessToken with your own
-    //add OSM base tilelayer
-            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//add tile layer...The Title I used was mapbox.outdoors. 
+            var outdoors = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
             maxZoom: 18,
             id: 'mapbox.outdoors',
             accessToken: 'pk.eyJ1IjoiZGZpZWxkMjMiLCJhIjoiY2p4NThuaGYxMDB3bDQ4cXd0eWJiOGJoeSJ9.T94xCeDwJ268CmzfMPXdmw'
             }).addTo(map);
+    
     //call getData function
     getData(map);
 }
@@ -35,10 +29,12 @@ function getData(map){
             createPropSymbols(response, map,attributes);
             //call funtion to create slider
             createSequenceControls(map, attributes);
+            //call function to create legend
+            createLegend(map, attributes); 
         }
     });
 }
-//Above Example 3.8...Step 3: build an attributes array from the data
+//Step 3: build an attributes array from the data
 function processData(data){
     //empty array to hold attributes
     var attributes = [];
@@ -48,9 +44,7 @@ function processData(data){
 
     //push each attribute name into attributes array
     for (var attribute in properties){
-        //only take the year attributes.
-        
-            if (attribute.indexOf("0")>=0){
+            if (attribute.indexOf("0")>=0){ //Grabs all of the Year Attributes 
             attributes.push(attribute);
         };
     };
@@ -139,7 +133,7 @@ function createSequenceControls(map, attributes){
         }
     });
 
-    map.addControl(new SequenceControl());
+map.addControl(new SequenceControl());
 
 
 	//set slider attributes
@@ -174,6 +168,24 @@ function createSequenceControls(map, attributes){
 	});
 }
 
+/*function createLegend(map, attributes){
+    
+    var LegendControl = L.Control.extend({
+        options: {
+            position: 'bottomright'
+        },
+
+        onAdd: function (map) {
+            // create the control container with a particular class name
+            var container = L.DomUtil.create('div', 'legend-control-container');
+            
+            
+            return container;
+        }
+    });
+
+map.addControl(new LegendControl());
+};*/
 
 //Step 10: Resize proportional symbols according to new attribute values
 function updatePropSymbols(map, attribute){
@@ -200,54 +212,5 @@ function updatePropSymbols(map, attribute){
     };
 });
     
-    function createLegend(map, attributes){
-    var LegendControl = L.Control.extend({
-        options: {
-            position: 'bottomright'
-        },
-
-        onAdd: function (map) {
-            // create the control container with a particular class name
-            var container = L.DomUtil.create('div', 'legend-control-container');
-
-            return container;
-        }
-    });
-
-    map.addControl(new LegendControl());
-};
-/*  //Example 2.7 line 1...function to create the legend
-function createLegend(map, attributes){
-    var LegendControl = L.Control.extend({
-        options: {
-            position: 'bottomright'
-        },
-
-        onAdd: function (map) {
-            // create the control container with a particular class name
-            var container = L.DomUtil.create('div', 'legend-control-container');
-
-            //add temporal legend div to container
-            $(container).append('<div id="temporal-legend">')
-            
-            
-            //Step 1: start attribute legend svg string
-            var svg = '<svg id="attribute-legend" width="180px" height="180px">';
-
-            //add attribute legend svg to container
-            $(container).append(svg);
-            
-/*            <svg id="Layer_1" width="180px" height="180px">
-            <circle fill = "#00BFFF" fill-opacity="0.8" stroke="#000000" stroke-miterlimit="10" cx="561" cy="321" r="89.5"/> 
-        </svg>*/
-/*            
-            return container;
-        }
-    });
-    map.addControl(createLegend);
-    map.addControl(new LegendControl());
-
-    updateLegend(map, attributes[0]);
-};  */
 };
 $(document).ready(createMap);
